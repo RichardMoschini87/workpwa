e0e79c6776f9f0a459efab35277d7110f0b7bd48
richard <moschini.richard@gmail.com>
2021-11-28 11:53:34 +0100

    fregtfrgtr
    fregtyt

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 
 function Header (){
     return(<div>
-     <h1 className="titolo"> <a className="hoverTitolo">BUILD WORKOUTS</a></h1>
+     <h1 className="titolo"> <a className="hoverTitolo">BUILD WORKOjjjjUTS</a></h1>
     </div>);
 }
 
