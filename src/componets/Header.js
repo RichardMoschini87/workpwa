4490e043eea7ee8b096ba237aaf5da024eb1c270
richard <moschini.richard@gmail.com>
2021-11-28 16:44:54 +0100

    commit

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 
 function Header (){
     return(<div>
-     <h1 className="titolo"> <a className="hoverTitolo">BUILD WORKOKOUT</a></h1>
+     <h1 className="titolo"> <a className="hoverTitolo">BUILDS WORKOKOUT</a></h1>
     </div>);
 }
 
