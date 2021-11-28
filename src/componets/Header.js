b931d215f352e58014a0ebb296cb3cbf3facd51d
richard <moschini.richard@gmail.com>
2021-11-28 11:39:44 +0100

    fce
    dvcdvf

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 
 function Header (){
     return(<div>
-     <h1 className="titolo"> <a className="hoverTitolo">BUILD WORKOUT</a></h1>
+     <h1 className="titolo"> <a className="hoverTitolo">BUILD WORKOUTS</a></h1>
     </div>);
 }
 
