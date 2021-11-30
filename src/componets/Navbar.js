9f76c9c40b211217a128ca9daf11a67562216d40
richard <moschini.richard@gmail.com>
2021-11-30 19:08:45 +0100

    navbar

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './Navbar.css'
+import { ItemNav } from '../navbarUtil/itemNav'
 
 function Navbar() {
-    return (<nav id='nav' class="navbar">
-        <a class="active">Home</a>
-        <a class="active">Insert</a>
-        <a class="active" >Search</a>
-        <a class="active">Workout</a>
-    </nav>);
+    return (
+        <nav id='nav' class="navbar">
+            {ItemNav.map((item, index) => {
+                return (
+                    <a class={item.cname} href={item.url}>{item.title}</a>
+                )
+            })}
+        </nav>);
 }
 
 export default Navbar;
---
+++ src/navbarUtil/itemNav.js
@@ -0,0 +1,17 @@
+export const ItemNav = [{
+    title: 'Home',
+    cname: 'active',
+    url: ''
+}, {
+    title: 'Insert',
+    cname: 'active',
+    url: ''
+}, {
+    title: 'Search',
+    cname: 'active',
+    url: ''
+}, {
+    title: 'Workout',
+    cname: 'active',
+    url: ''
+}]
