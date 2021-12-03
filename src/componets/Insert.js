5e4f4feec6b5b2f45677806ed2a4e15d31c98cd4
RichardMoschini87 <62137330+RichardMoschini87@users.noreply.github.com>
2021-12-03 11:58:21 +0100

    cronometro prova

--- src/componets/Insert.js
+++ src/componets/Insert.js
@@ -1,12 +1,37 @@
 import React from "react";
 import './Insert.css'
 
-function Insert(){
-    //conflitto
-    return(
-        <div id='div'><h1>Componente di Insert component</h1></div>
+function Insert() {
+    var cronometro = 0;
+    var min = 0, sec = 0
+
+    const start = () => {
+        cronometro = setInterval(() => {
+            sec++;
+            if (sec === 60) {
+                min++;
+                sec = 0;
+            }
+            document.getElementById('idd').innerHTML = min + ':' + sec
+        }, 1000)
+    }
+
+    const stop = () => {
+        clearInterval(cronometro)
+        min = 0;
+        sec = 0;
+        document.getElementById('idd').innerHTML = min + ':' + sec
+    }
+
+    return (
+        <div>
+            <h1 id='idd'></h1>
+            <button onClick={start} >Start</button>
+            <button onClick={stop}>STOP</button>
+        </div>
     )
-	//conflittos
 }
 
 export default Insert;
+
+
